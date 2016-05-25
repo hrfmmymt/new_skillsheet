5aa036fa7ee645c179085a4dcad9c5bee918b16c
hrfmmymt <hrfmmymt@gmail.com>
2016-05-26 01:06:44 +0900

    use jsx.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module NewSkillsheet
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.browserify_rails.commandline_options = '-t babelify'
+    config.browserify_rails.commandline_options = '-t babelify --extension=.jsx'
   end
 end
